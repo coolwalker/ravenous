27296d9992820944d764504a3f4d3e062f36929d
Abhishek <coolwalkar@Abhisheks-MacBook-Pro.local>
2018-06-24 19:25:13 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from '../src/components/BusinessList/BusinessList';
+import SearchBar from '../src/components/SearchBar/SearchBar';
+import Business from './components/Business/Business';
+import Yelp from '../src/util/Yelp';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    if(term.length <= 0 || location.length <= 0){
+      return;
+  }
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp = {this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses}/>
+      </div>
+    );
+  }
+}
+
+export default App;
